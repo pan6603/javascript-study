cbdad06770545ee576be5df02a7c9716e9dfc2ff
pan6603 <pan5158a@gmail.com>
2025-01-12 08:48:37 +0900

    toString() 사용방법

---
+++ today/20250112.js
@@ -0,0 +1,19 @@
+// toString() 사용방법 
+
+// 숫자를 문자열로 변환
+// let num = 123; 
+// let str = num.toString();
+// console.log(str);
+// console.log(typeof str);
+
+// 배열을 문자열로 변환 
+let arr = [1, 2, 3];
+let str = arr.toString();
+console.log(str);
+console.log(typeof str);
+
+let bool = true; 
+let bool2 = bool.toString();
+console.log(bool2);
+console.log(typeof bool2);
+
