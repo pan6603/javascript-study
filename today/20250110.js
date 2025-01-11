7f12a959cd7cf862904d1ce5f053fd8de9534dcb
pan6603 <pan5158a@gmail.com>
2025-01-11 09:30:21 +0900

    배열 push와 pop() 사용법

--- today/20250110.js
+++ today/20250110.js
@@ -1,10 +1,21 @@
 // 문자열 정렬
-let fruits = ["Banana", "Apple", "Cherry"];
-fruits.sort();
-console.log(fruits);
+// let fruits = ["Banana", "Apple", "Cherry"];
+// fruits.sort();
+// console.log(fruits);
 
-// 숫자 정렬 
-let numbers = [40, 1, 5, 200];
-numbers.sort();
-console.log(numbers);
+// // 숫자 정렬 
+// let numbers = [40, 1, 5, 200];
+// numbers.sort();
+// console.log(numbers);
 
+// 배열 push사용법 
+// let arr = [1, 2, 3];
+// arr.push(4);
+// console.log(arr);
+
+
+//배열 pop() 사용법 
+let arr = [1, 2, 3];
+let removedElement = arr.pop();
+console.log(removedElement);
+console.log(arr);
