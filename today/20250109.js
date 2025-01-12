6ae3c54e560a4ab59be1c31ae13624cec7970137
pan6603 <pan5158a@gmail.com>
2025-01-12 09:06:45 +0900

    push와 pop 사용법

--- today/20250109.js
+++ today/20250109.js
@@ -3,3 +3,14 @@ console.log(arr.length);
 console.log(arr[0]);
 console.log(arr[2]);
 console.log(arr[8]);
+
+// push() 사용법 
+let arr1 = [1, 2, 3];
+arr1.push(4);
+console.log(arr1);
+
+// pop() 사용법
+let arr2 = [1, 2, 3, 4];
+let removedElement = arr2.pop();
+console.log(arr2);
+console.log(removedElement);
