e151517558869190240b6be10ca55637e74396cd
pan6603 <pan5158a@gmail.com>
2025-01-13 08:22:50 +0900

    자료형 변환 사용법

---
+++ today/20250113.js
@@ -0,0 +1,16 @@
+// 숫자 변환 
+console.log(Number("123"));
+console.log(Number("abc"));
+console.log(Number(true));
+console.log(Number(false));
+
+// 문자열 변환 
+console.log(String(123));
+console.log(String(true));
+console.log(String(false));
+console.log(String(null));
+console.log(String(undefined));
+
+const str = "123";
+const num = Number(str);
+console.log(num + 1);
