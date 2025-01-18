1d713949e3dae1653e59eb293b4751bce7c87ab2
pan6603 <pan5158a@gmail.com>
2025-01-18 10:34:39 +0900

    문자열, 배열 includes() 메소드 사용

--- today/20250118-1.js
+++ today/20250118-1.js
@@ -1,7 +1,17 @@
-let str = "Hello, world";
-let index = str.indexOf("world");
-console.log(index);
+// let str = "Hello, world";
+// let index = str.indexOf("world");
+// console.log(index);
 
-let arr = [10, 20, 30, 40];
-let index1 = arr.indexOf(30);
-console.log(index1);
+// let arr = [10, 20, 30, 40];
+// let index1 = arr.indexOf(30);
+// console.log(index1);
+
+// includes() 메소드 사용방법 
+
+let str = "Hello, world";
+let result = str.includes("world");
+console.log(result);
+
+let arr = [10, 20, 30, 40, 50];
+let result1 = arr.includes(30);
+console.log(result1);
