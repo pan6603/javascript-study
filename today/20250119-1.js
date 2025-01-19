977035c812e741c5927b52a60e33ff41be67fcf4
pan6603 <pan5158a@gmail.com>
2025-01-19 09:41:17 +0900

    join() 메소드 사용

---
+++ today/20250119-1.js
@@ -0,0 +1,9 @@
+// const fruits = ['apple', 'banana', 'cherry'];
+
+// console.log(fruits.join());
+// console.log(fruits.join(' - '));
+// console.log(fruits.join(''));
+
+const fruits = ["Apple", "Banana", "Cherry"];
+const result = fruits.join();
+console.log(result);
