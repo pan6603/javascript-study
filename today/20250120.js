7cdbf609d537483d96dc7ad6bd38ea686bfbc28a
pan6603 <pan5158a@gmail.com>
2025-01-20 11:24:48 +0900

    배열 split() 메소드 사용

---
+++ today/20250120.js
@@ -0,0 +1,12 @@
+// const str = "apple,banana,cherry";
+// const result = str.split(",");
+// console.log(result);
+
+// const str = "hello";
+// const result = str.split("");
+// console.log(result);
+
+const str = "hello";
+const result = str.split();
+console.log(result);
+
