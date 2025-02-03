55e17c2e0a7019fcb735832f3b5de4d626ccd87c
pan6603 <pan5158a@gmail.com>
2025-02-03 11:54:29 +0900

    indexOf()메소드 사용

---
+++ today/02/20250203.js
@@ -0,0 +1,12 @@
+// 배열에서 indexOf() 메소드 사용 
+const arr = [10, 20, 30, 40, 50];
+
+console.log(arr.indexOf(30));
+console.log(arr.indexOf(60));
+
+//문자열에서 indexOf() 사용 
+const str = "Hello, World";
+
+console.log(str.indexOf("World"));
+console.log(str.indexOf("JavaScript"));
+
