cfee27ff53c2a118a1b57fc0ad8abf6ea0abcd16
pan6603 <pan5158a@gmail.com>
2025-02-04 14:27:27 +0900

    함수 호이스팅 사용방법

--- today/02/20250204.js
+++ today/02/20250204.js
@@ -1,3 +1,12 @@
-let text = "Hello (world)! (Nice) to meet (you)";
-let result = text.replaceAll("(", "[");
-console.log(result);
+// let text = "Hello (world)! (Nice) to meet (you)";
+// let result = text.replaceAll("(", "[");
+// console.log(result);
+
+
+// 함수 호이스팅 사용하기 
+
+function sayHello() {
+  console.log("Hello");
+}
+
+sayHello();
